19d973e85e8688dc585c78fe432e9d6117311f7a
Oleksiy Makarov <Oleksiy Makarov>
2017-08-05 11:54:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,124 @@
+var _ = require('lodash');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 80;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+var scanners = [];
+var controllers = [];
+var mainTrigger = null;
+
+
+
+
+
+io.on('connection', function(socket){
+
+  // add clients
+
+
+    socket.on('disconnect', function(){
+
+        //console.log("disconnect", JSON.stringify(socket));
+        var pos = scanners.indexOf(socket);
+        if(pos != -1){
+            scanners.splice(pos, 1);
+            console.log("disconnected scanner. IP: " + socket.scanner.ip);
+        }
+        var pos = controllers.indexOf(socket);
+        if(pos != -1){
+            controllers.splice(pos, 1);
+            console.log("disconnected controller");
+        }
+
+        if(mainTrigger === socket){
+            mainTrigger = null;
+            console.log("disconnected trigger");
+        }
+
+        updateControllers();
+
+
+        //io.emit('chat message', msg);
+    });
+
+  socket.on('add scanner', function(scanner){
+	socket.scanner = scanner;
+	scanners.push(socket);
+      updateControllers();
+
+      console.log("added scanner. IP: " + scanner.ip);
+    //io.emit('chat message', msg);
+  });
+
+  socket.on('add controller', function(controller){
+	socket.controller = controller;
+	controllers.push(socket);
+      updateControllers();
+
+      console.log("added controller");
+    //io.emit('chat message', msg);
+  });
+
+  socket.on('add trigger', function(trigger){
+	socket.trigger = trigger;
+	mainTrigger = socket;
+      updateControllers();
+
+      console.log("added trigger");
+    //io.emit('chat message', msg);
+  });
+
+  // 
+
+
+  socket.on('setup command', function(cmd){
+	for(var i = 0; i < scanners.length; i++){
+        scanners[i].emit('setup command', cmd);
+    }
+      //updateControllers();
+  });
+
+    socket.on('start command', function(cmd){
+        if(mainTrigger){
+            mainTrigger.emit('start command', cmd);
+        }
+        //updateControllers();
+    });
+
+    socket.on('update data', function(cmd){
+        updateControllers();
+    });
+
+});
+
+
+function updateControllers(){
+
+    var data = {
+        scanners: _.map(scanners, function(scanner){
+            return{
+                id: scanner.id,
+                data: scanner.scanner
+            }
+        }),
+        trigger: mainTrigger? {
+                id: mainTrigger.id,
+                data: mainTrigger.trigger
+            }: null
+    };
+
+    for(var i = 0; i < controllers.length; i++){
+        controllers[i].emit('update data', data);
+    }
+}
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
---
+++ scanerPI/server.js
@@ -0,0 +1,131 @@
+var shell = require('shelljs');
+var gpio = require('rpi-gpio');
+var ip = require('ip');
+var s = require('socket.io-client');
+
+
+
+
+/*var Raspistill = require('node-raspistill').Raspistill;
+var camera = new Raspistill({
+	time: 10
+});*/
+
+
+var rpiIp = ip.address() // my ip address
+
+
+var command = "";
+
+
+var socket = s('http://192.168.1.217');
+
+
+socket.on('connect', function(c){
+	console.log("connected to server");
+
+	socket.emit("add scanner", {
+		ip: rpiIp, 
+		files:[]
+	});
+});
+socket.on('setup command', function(data){
+	command = data;
+	console.log("setup command:", data);
+});
+socket.on('disconnect', function(e){
+	console.log("disconnected from server");
+
+});
+
+
+var RaspiCam = require("raspicam");
+
+var opts = {
+	mode: "timelapse",
+	tl: 150,
+	t: 600,
+	n: true,
+	output: "test.jpg",
+	h: 1920,
+	w: 1024
+};
+
+var camera = new RaspiCam(opts);
+
+
+
+//listen for the "start" event triggered when the start method has been successfully initiated
+camera.on("start", function(){
+	//do stuff
+});
+
+
+camera.on("read", function(err, timestamp, filename){ 
+	console.log(">>>>>>>>>>>>>>>>>>>>");
+
+	console.log(new Date());
+});
+
+
+//listen for the "stop" event triggered when the stop method was called
+camera.on("stop", function(){
+	//do stuff
+});
+
+//listen for the process to exit when the timeout has been reached
+camera.on("exit", function(){
+	//do stuff
+});
+
+console.log("Scanner started");
+console.log("Current ip: " + rpiIp);
+
+var attempt = 0;
+
+gpio.on('change', function(channel, value) {
+	
+	if(command !== "" && channel == 7 && value == 1){
+
+		console.log("execute command: " + command);
+
+		attempt ++;
+//		gpio.write(12, true);
+//		shell.exec(command + attempt + '.jpg');
+
+
+		camera.start();
+
+
+/*		var array = [];
+
+		camera.timelapse(1000, 2800, (image) => {
+
+			console.log("test");
+			console.log(new Date());
+
+//			array.push(image);
+		
+		}).then(() => {
+			console.log("end");
+
+		}).catch((err) => {
+			console.log("err",err);
+		});*/
+
+
+		setTimeout(function(){
+			gpio.write(12, false);
+		},300);		
+	}
+
+	//console.log('Channel ' + channel + ' value is now ' + value);
+});
+ 
+gpio.setup(12, gpio.DIR_OUT, write);
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+
+function write() {
+	gpio.write(12, false);
+}
