7c980e98d3cab03cf61dc36ac3b13aaaedb75c02
omakarov <omakarov@us.imshealth.com>
2017-11-15 01:13:33 +0200

    bug fixes

--- daemon/daemon.js
+++ daemon/daemon.js
@@ -1,85 +1,82 @@
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 
+var inputFolder = 'c:\\example\\';
+var monitorFolder = 'c:\\monitor\\';
 
-var initJSONLocation = "c:\\monitor\\example.json"
-var doneJSONLocation = "c:\\monitor\\example.json.done"
+var inputJSONName = 'example.json';
+var doneJSONName = 'example.json.done';
 
-
-var inputJSONName = "example.json"
-var inputFolder = "c:\\example\\"
+var initJSONLocation = monitorFolder + inputJSONName;
+var doneJSONLocation = monitorFolder + doneJSONName;
 
 var searching = false;
 
-function searchNewData(){
-  fs.readdir(inputFolder, function(err, items){
-
-
-    if(err){
-    	console.log("Error daemon", err)
-		return;
+function searchNewData() {
+  fs.readdir(inputFolder, function(err, items) {
+    if (err) {
+      console.log('Error daemon', err);
+      return;
     }
 
-    console.log("try to search new JSON")
-    for(var i = 0; i < items.length; i++){
-
-      var folder = inputFolder + items[i] + "\\";
+    var newData = false;
+    console.log('try to found new JSON');
+    for (var i = 0; i < items.length; i++) {
+      var folder = inputFolder + items[i] + '\\';
       var newJSON = folder + inputJSONName;
-      if(fs.existsSync(newJSON)){
+      var doneJSON = folder + doneJSONName;
 
-        console.log("new JSON is found")
-
+      if (fs.existsSync(newJSON) && !fs.existsSync(doneJSON)) {
+        newData = true;
+        console.log('New JSON is found');
         copyJSON(newJSON, folder);
-
-        //searching = false;
         break;
       }
     }
 
-    searching = false;
+    if (!newData) {
+      searching = false;
+      console.log('New data not found');
+    }
   });
 }
 
-
-function copyJSON(newJSON, folder){
-
-  // remove done JSON
-  fs.unlink(doneJSONLocation, function(){
-  });
-
-  // copy new JSON to init directory
+function copyJSON(newJSON, folder) {
+  // copy new JSON to Builder directory
   var ws = fs.createWriteStream(initJSONLocation);
-  ws.on("close", function(){
+  ws.on('close', function() {
+    console.log('JSON copied into init folder');
+    fs.createReadStream(newJSON).pipe(fs.createWriteStream(newJSON + '.done'));
 
-    console.log("JSON copied into init folder");
-
-    setTimeout(function(){
+    setTimeout(function() {
       searching = false;
-
-      // Rename original file
-      fs.rename(newJSON, folder + "example.done.json", function(){
-      });
-    },500);
-
+    }, 50);
   });
+
   fs.createReadStream(newJSON).pipe(ws);
 }
 
-function checkJSONDone(callback){
+function checkJSONDone(callback) {
   // check is done exist
-  if(fs.existsSync(doneJSONLocation)){
-    console.log("JSON done")
+  if (fs.existsSync(doneJSONLocation)) {
+    fs.unlink(doneJSONLocation, function() {});
+    console.log('Builder is ready');
     searching = true;
     callback();
+  } else if (!fs.existsSync(initJSONLocation)) {
+    console.log('Builder is ready');
+    searching = true;
+    callback();
+  } else {
+    console.log('Builder is busy');
   }
 }
 
-setInterval(function(){
-
-  if(!searching) {
-    console.log("check is JSON done")
-    checkJSONDone(function(){
+setInterval(function() {
+  if (!searching) {
+    console.log('Check builder');
+    checkJSONDone(function() {
       searchNewData();
     });
   }
-}, 3000)
+}, 5000);
