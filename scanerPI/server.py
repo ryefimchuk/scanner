7fd7b093759f0ea8d576a0dbc50c01cad2bbc75d
oleksii makarov <oleksii.makarov@privishealth.com>
2018-06-22 00:54:57 +0300

    update busy indicator

--- scanerPI/server.py
+++ scanerPI/server.py
@@ -195,12 +195,14 @@ class SocketHandler:
 
     def executeShell(self, data):
         cmd = json.loads(data)
+        self.updateBusyState(True)
         try:
             process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
             for line in process.stdout:
                 print(line.decode('utf-8'))
         except Exception as e:
             print("Error execute shell({0}): {1}".format(e.errno, e.strerror))
+        self.updateBusyState(False)
 
     def getScannerNumber(self):
         try:
